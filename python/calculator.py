bac2231c995656b23691a11cc2853a13f4f82e81
l0ksh <lokesh904@gmail.com>
2023-06-27 22:33:53 +0530

    Added comments to calculator.py

--- python/calculator.py
+++ python/calculator.py
@@ -1,18 +1,24 @@
 #!/bin/usr/python3
 
-# This is a basic calculator
+#This is a basic calculator
 
+
+#Taking inputs from user
 var1 = input('Enter first number: ')
 var2 = input('Enter second number: ')
 
+#Addition
 sum = int(var1) + int(var2)
 print('The sum is:',sum)
 
+#Subtraction
 sub = int(var1) - int(var2)
 print('The subtraction is:', sub)
 
+#Multiplication
 multiply = int(var1) * int(var2)
 print('The multiplication is:', multiply)
 
+#Division
 division = int(var1) / int(var2)
 print('The division is:', division)
